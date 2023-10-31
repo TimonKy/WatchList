6e00687ac99da195bbd8d7e7ed9f8aee93635bc8
TimonKy <timon.kybranz@gmail.com>
2023-10-31 11:14:19 +0100

    speicher

---
+++ src/routes/About.svelte
@@ -0,0 +1,12 @@
+<script>
+    // Ihr Skript hier
+  </script>
+  
+  <style>
+    /* Ihr Stil hier */
+  </style>
+  
+  <main>
+    <h1>About Page</h1>
+    <p>This is the about page.</p>
+  </main>
---
+++ src/routes/test.svelte
@@ -0,0 +1,27 @@
+<div class="index">
+    <h1>Website</h1>
+</div>
+<ul class="nav nav-tabs">
+    <li class="nav-item">
+      <a class="nav-link active" href="#">Home</a>
+    </li>
+    <li class="nav-item dropdown">
+      <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Watchlist</a>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Anime</a>
+        <a class="dropdown-item" href="#">Serie</a>
+        <a class="dropdown-item" href="#">Filme</a>
+      </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link disabled" href="#">About Me</a>
+      </li>
+  </ul>
+
+  <style>
+    .index {
+        padding: 20px;
+        display: block;
+        text-align: center;
+    }
+  </style>
