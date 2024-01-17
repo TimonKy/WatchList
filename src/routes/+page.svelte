c84fa1cf5879daaa4ecdf984833342e70ccc1017
TimonKy <timon.kybranz@gmail.com>
2024-01-17 18:31:30 +0100

    fixed the header and footer + added a layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,3 @@
-<nav>
-  <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="./about.svelte">About</a></li>
-      <li><a href="/contact">Watchlist</a></li>
-  </ul>
-</nav>
-<h1>  </h1>
-
-
-<style>
-  nav {
-      background-color: #5b5a5a;
-      color: white;
-      padding: 10px;
-  }
-  
-  nav ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-      overflow: hidden;
-  }
-  
-  nav li {
-      float: left;
-  }
-  
-  nav li a {
-      display: block;
-      color: white;
-      text-align: center;
-      padding: 6px 16px;
-      text-decoration: none;
-  }
-</style>
+<footer>
+    <p>hallo</p>
+</footer>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,44 @@
+<header>
+    <nav>
+        <h1>Web</h1>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/watchlist">Watchlist</a></li>
+        </ul>
+  </nav>
+</header>
+  
+  <style>
+    header {
+        background-color: #333;
+        color: white;
+        padding: 1rem;
+        text-align: center;
+    }
+    nav {
+        background-color: #333;
+        color: white;
+        padding: 10px;
+    }
+    
+    nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+    }
+    
+    nav li {
+        float: left;
+    }
+    
+    nav li a {
+        display: block;
+        color: white;
+        text-align: center;
+        padding: 6px 16px;
+        text-decoration: none;
+    }
+  </style>
+  
